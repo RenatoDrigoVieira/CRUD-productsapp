6571cff47a1d24c0fc8c840ef18bd9dac5920779
unknown <renatosbc3434@gmail.com>
2019-08-21 15:58:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductComponent } from './product/product.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    DepartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/department/department.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { Department } from '../department';
+import { DepartmentService } from '../department.service';
+import { filter } from 'minimatch';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html',
+  styleUrls: ['./department.component.css']
+})
+export class DepartmentComponent implements OnInit {
+
+  depName: string = ''
+  depEdit: Department = null
+  departments: Department[] = []
+  constructor(private departmentService: DepartmentService,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.departmentService.get()
+    .subscribe((deps)=>{
+      this.departments = deps
+    })
+  }
+
+  save(){
+    if(this.depEdit){
+      this.departmentService.update({name: this.depName, _id:this.depEdit._id})
+      .subscribe((dep)=>{
+        this.notify('Updated')
+      },
+      (err)=>{
+        this.notify('Error')
+        console.error(err)
+      })
+
+
+    }else{
+      console.log('asdasd')
+      this.departmentService.add({name: this.depName})
+        .subscribe((dep)=>{
+          this.notify('Insertedd')
+          console.log(dep)
+          
+        },
+        (err)=>console.log(err))
+        this.clear()
+
+    }
+  }
+  clear(){
+    this.depName=''
+    this.depEdit = null
+  }
+  cancel(){
+
+  }
+  delete(dep: Department){
+    this.departmentService.del(dep)
+      .subscribe(
+        ()=> this.notify('Removed'),
+        (err)=>console.log(err)
+      )
+  }
+  edit(dep: Department){
+    this.depName = dep.name
+    this.depEdit = dep
+  }
+  notify(msg: string){
+    this.snackBar.open(msg,'OK',{duration:3000})
+  }
+
+}
